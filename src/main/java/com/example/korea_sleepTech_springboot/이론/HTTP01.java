5aa4b568a930ed4adbe1de7754c40684631e0b28
Jangsojeong31 <sojung301@naver.com>
2025-04-28 12:38:52 +0900

    250428 springboot commit

---
+++ src/main/java/com/example/korea_sleepTech_springboot/이론/HTTP01.java
@@ -0,0 +1,13 @@
+package com.example.korea_sleepTech_springboot.이론;
+
+public class HTTP01 {
+
+    /*
+    === 요청(Request) ===
+        : 클라이언트가 서버에게 전달
+        - URL: 어디에 요청할지
+        - 메서드: 어떤 종류의 요청인지 (EX. GET, POST 등)
+        - 헤더(Header): 추가 정보('클라이언트 정보', '응답데이터에 대한 형식' 등)
+        - 바디(
+     */
+}
