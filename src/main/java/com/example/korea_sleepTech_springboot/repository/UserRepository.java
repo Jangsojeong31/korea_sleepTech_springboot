d9269d725d7d97b0917471da81032f9c50f83fa4
Jangsojeong31 <sojung301@naver.com>
2025-05-12 11:40:14 +0900

    20250512 User/Auth

---
+++ src/main/java/com/example/korea_sleepTech_springboot/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.example.korea_sleepTech_springboot.repository;
+
+import com.example.korea_sleepTech_springboot.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
